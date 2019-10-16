d0a5451f4c6360f187693d80be7202c65d42ba68
Jeff Neuberger <jeffreyneuberger@gmail.com>
2019-10-16 18:51:12 -0400

    Add support for changing break length with buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,25 @@ import React from 'react';
 import './App.css';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: 5,
+      sessionLength: 25
+    }
+  }
+
+  breakUp = () => {
+    if(this.state.breakLength <= 59) { 
+      this.setState( {breakLength: this.state.breakLength + 1} );
+    }
+  }
+
+  breakDown = () => {
+    if(this.state.breakLength >= 2) {
+      this.setState( {breakLength: this.state.breakLength - 1} )
+    }
+  }
 
   render() {
     return (
@@ -11,14 +30,14 @@ export default class App extends React.Component {
           <div id="label-section">
             <div id="break-label" className="label">
               <h3>Break Label</h3>
-              <button id="break-increment">Increase</button>
-              <div id="break-length">5</div>
-              <button id="break-decrement">Decrease</button>
+              <button id="break-increment" onClick={this.breakUp}>Increase</button>
+              <div id="break-length">{this.state.breakLength}</div>
+              <button id="break-decrement" onClick={this.breakDown}>Decrease</button>
             </div>
             <div id="session-label" className="label">
               <h3>Session Label</h3>
               <button id="session-increment">Increase</button>
-              <div id="session-length">25</div>
+              <div id="session-length">{this.state.sessionLength}</div>
               <button id="session-decrement">Decrease</button>
             </div>
           </div>
