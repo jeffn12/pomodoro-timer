bf3747aadd38d3fbb6469f5f26a18fb0e822178d
Jeff Neuberger <jeffreyneuberger@gmail.com>
2019-10-21 14:07:04 -0400

    Fix conditional comparitor operator

--- src/components/TimerDisplay.js
+++ src/components/TimerDisplay.js
@@ -4,7 +4,7 @@ export default class TimerDisplay extends React.Component {
     getTimeFormat = () => {
         const arr = this.props.time.split(':');
         const min = arr[0] < 10 ? '0' + arr[0].toString() : arr[0].toString();
-        const sec = ( arr[1] < 10 & arr[1] != '00' ) ? '0' + arr[1].toString() : arr[1].toString();
+        const sec = ( arr[1] < 10 & arr[1] !== '00' ) ? '0' + arr[1].toString() : arr[1].toString();
         return min + ":" + sec;
     }
 
