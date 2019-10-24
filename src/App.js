f573e3c4077cb1c2f0a79304579d013ffa1a02eb
jeffn12 <55459088+jeffn12@users.noreply.github.com>
2019-10-24 10:07:32 -0400

    Fix radix warning on parseInt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import TimerSettings from './components/TimerSettings';
-import TimerDisplay from './components/TimerDisplay';
-import TimerControls from './components/TimerControls';
-import './App.css';
+import React from "react";
+import TimerSettings from "./components/TimerSettings";
+import TimerDisplay from "./components/TimerDisplay";
+import TimerControls from "./components/TimerControls";
+import "./App.css";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -12,111 +12,117 @@ export default class App extends React.Component {
       sessionLength: 25,
       timerRun: false,
       time: "25:00",
-      timerType: "Session",
-    }
+      timerType: "Session"
+    };
   }
 
   breakUp = () => {
-    if(this.state.breakLength <= 59 && !this.state.timerRun) { 
-      this.setState( {breakLength: this.state.breakLength + 1} );
+    if (this.state.breakLength <= 59 && !this.state.timerRun) {
+      this.setState({ breakLength: this.state.breakLength + 1 });
     }
-  }
+  };
 
   breakDown = () => {
-    if(this.state.breakLength >= 2 && !this.state.timerRun) {
-      this.setState( {breakLength: this.state.breakLength - 1} )
+    if (this.state.breakLength >= 2 && !this.state.timerRun) {
+      this.setState({ breakLength: this.state.breakLength - 1 });
     }
-  }
+  };
 
   sessionUp = () => {
-    if(this.state.sessionLength <= 59 && !this.state.timerRun) { 
-      this.setState( {sessionLength: this.state.sessionLength + 1, time: `${this.state.sessionLength + 1}:00` });
+    if (this.state.sessionLength <= 59 && !this.state.timerRun) {
+      this.setState({
+        sessionLength: this.state.sessionLength + 1,
+        time: `${this.state.sessionLength + 1}:00`
+      });
     }
-  }
+  };
 
   sessionDown = () => {
-    if(this.state.sessionLength >= 2 && !this.state.timerRun) {
-      this.setState( {sessionLength: this.state.sessionLength - 1, time: `${this.state.sessionLength - 1}:00` } )
+    if (this.state.sessionLength >= 2 && !this.state.timerRun) {
+      this.setState({
+        sessionLength: this.state.sessionLength - 1,
+        time: `${this.state.sessionLength - 1}:00`
+      });
     }
-  }
+  };
 
   reset = () => {
     this.timerStop();
-    this.setState( {
+    this.setState({
       breakLength: 5,
       sessionLength: 25,
       timerRun: false,
       time: "25:00",
       timerType: "Session"
     });
-  }
+  };
 
   timerStart = () => {
-    this.setState( {
-      timerRun: true,
-    }, function() { 
-      const arr = this.state.time.split(':');
-      const sec = parseInt(arr[0]) * 60 + parseInt(arr[1]);
-      this.start(sec);}  
+    this.setState(
+      {
+        timerRun: true
+      },
+      function() {
+        const arr = this.state.time.split(":");
+        const sec = parseInt(arr[0], 10) * 60 + parseInt(arr[1], 10);
+        this.start(sec);
+      }
     );
-    
-  }
+  };
 
-  start = (sec) => {
-    if(this.state.timerRun) {
-      let x = setInterval( () => {
-        sec--; 
-        if(!this.state.timerRun || sec < 0) { 
+  start = sec => {
+    if (this.state.timerRun) {
+      let x = setInterval(() => {
+        sec--;
+        if (!this.state.timerRun || sec < 0) {
           clearInterval(x);
-          if(sec < 0) {
+          if (sec < 0) {
             this.endTimer();
           }
-        }
-        else {
+        } else {
           this.tick(sec);
         }
-      }, 1000); 
-      
+      }, 1000);
     }
-  }
+  };
 
-  tick = (sec) => {
-    this.setState( {
-      time: `${Math.floor(sec / 60).toString()}:${(sec % 60).toString()}`,
-    } );
-  }
+  tick = sec => {
+    this.setState({
+      time: `${Math.floor(sec / 60).toString()}:${(sec % 60).toString()}`
+    });
+  };
 
   endTimer = () => {
-    switch(this.state.timerType) {
-      case 'Session':
-        this.setState( {
-          timerType: 'Break',
-          time: this.state.breakLength.toString() + ":00",
+    switch (this.state.timerType) {
+      case "Session":
+        this.setState({
+          timerType: "Break",
+          time: this.state.breakLength.toString() + ":00"
         });
         break;
-      case 'Break':
-        this.setState( {
-          timerType: 'Session',
-          time: this.state.sessionLength.toString() + ":00",
+      case "Break":
+        this.setState({
+          timerType: "Session",
+          time: this.state.sessionLength.toString() + ":00"
         });
         break;
       default:
-        alert('Timer broken');
+        alert("Timer broken");
         break;
     }
     this.timerStart();
-  }
+  };
 
   timerStop = () => {
-    this.setState( {
-      timerRun: false,
+    this.setState({
+      timerRun: false
     });
-  }
+  };
 
   render() {
     return (
       <div id="wrapper">
-        <TimerSettings 
+        <TimerSettings
           breakUp={this.breakUp}
           breakDown={this.breakDown}
           sessionUp={this.sessionUp}
@@ -124,15 +130,14 @@ export default class App extends React.Component {
           breakLength={this.state.breakLength}
           sessionLength={this.state.sessionLength}
         />
-        <TimerDisplay 
-          name={this.state.timerType}
-          time={this.state.time}
-        />  
+        <TimerDisplay name={this.state.timerType} time={this.state.time} />
         <TimerControls
-          runToggle={this.state.timerRun === true ? this.timerStop : this.timerStart }
+          runToggle={
+            this.state.timerRun === true ? this.timerStop : this.timerStart
+          }
           reset={this.reset}
         />
-      </div>   
-    )
+      </div>
+    );
   }
 }
